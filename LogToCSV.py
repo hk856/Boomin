934b55cfc839fc0827f7fe6621462b6015cc8459
hk856 <hk856@cornell.edu>
2016-09-20 17:07:54 -0400

    Convert log to csv in 220 sec

--- LogToCSV.py
+++ LogToCSV.py
@@ -1,19 +1,28 @@
-
 import csv
+import time
+import os
 
 columns = {"cid","fid", "pth", "ref", "sttp", "uri", "uuid", "us", "ts", "ip", "r", "cuuid", "ae", "ancestors", "from", "mid", "mnis", "sid", "ti", "path", "action", "cnt", "dewey", "drop", "cf", "lmap", "mnid", "pid", "ua"}
 
 
-with open('Data/test.txt','r') as fin:
+os.chdir('..')
+starttime = time.time()
+print('starttime: %d' %starttime)
+with open('Data/scratch_2016_06_17_kavula.log.txt','r') as fin:
 
     # reader = csv.reader(fin)
     reader = fin.readlines()
 
-    with open('testout.csv,','w') as output:
+    with open('testout.csv','w') as output:
         dict_writer = csv.DictWriter(output, columns, extrasaction='ignore')
         dict_writer.writeheader()
         for item in reader:
-            print(eval(item))
-            print('\n')
+            item = item.replace('null', 'None')
+            # print(item)
+            # print(eval(item))
+            # print('\n')
             dict_writer.writerow(eval(item))
 
+endtime = time.time()
+print('endtime: %d' %endtime)
+print('time used: %d' %(endtime-starttime))
