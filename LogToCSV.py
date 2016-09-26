8848f4b9001c17bcbd2e6f5a6ad02e0bb64e77a3
hk856 <hk856@cornell.edu>
2016-09-25 20:47:34 -0400

    added some functions in Tree class

--- LogToCSV.py
+++ LogToCSV.py
@@ -2,26 +2,26 @@ import csv
 import time
 import os
 
-columns = {"fid", "ref", "sttp", "uri", "ts", "ip", "ancestors", "mid", "ti", "ua"}
+columns = {"fid", "ref", "sttp", "uri", "ts", "ip", "ancestors", "mid", "ti", "ua", "uuid"}
 
 
 os.chdir('..')
 starttime = time.time()
 print('starttime: %d' %starttime)
-with open('desktop/data/data.log','r') as fin:
+with open('Data/scratch_2016_06_17_kavula.log.txt','r') as fin:
 
-    # reader = csv.reader(fin)
     reader = fin.readlines()
 
-    with open('testout.csv','w') as output:
+    with open('data.csv','w') as output:
         dict_writer = csv.DictWriter(output, columns, extrasaction='ignore')
         dict_writer.writeheader()
         for item in reader:
             item = item.replace('null', 'None')
-            # print(item)
-            # print(eval(item))
-            # print('\n')
-            dict_writer.writerow(eval(item))
+            newitem = eval(item)
+            if 'uri' in newitem:
+                newitem['uri'] = 'https://www.skroutz.gr' + newitem['uri']
+            # print(newitem)
+            dict_writer.writerow(newitem)
 
 endtime = time.time()
 print('endtime: %d' %endtime)
--- MPSBigDataCleanUp/src/UserPathTreeNode.java
+++ MPSBigDataCleanUp/src/UserPathTreeNode.java
@@ -1,28 +1,56 @@
 import java.util.ArrayList;
 
 public class UserPathTreeNode {
-	private String URI;
+	private String fid;
+	private String uri;
 	private String ref;
 	private String sttp;
 	private String ts;
 	private String ip;
+	private String ancestors;
+	private String mid; 
+	private String ti;
+	private String ua;
+	private String uuid;
 	private ArrayList<UserPathTreeNode> children;
-	private String parent;
+	private UserPathTreeNode parent = null;
 	
-	public UserPathTreeNode(String URI, String ref, String sttp, String ts, String ip){
-		this.URI = URI;
+	/**
+	 * Input following values in order to construct a new node:
+	 * "fid", "ref", "sttp", "uri", "ts", "ip", "ancestors", "mid", "ti", "ua", "uuid"
+	**/
+	public UserPathTreeNode(String fid, String ref, String sttp, String uri, String ts, String ip,
+		String ancestors, String mid, String ti, String ua, String uuid){
+		this.fid = fid;
+		this.uri = uri;
 		this.ref = ref;
 		this.sttp = sttp;
 		this.ts = ts;
 		this.ip = ip;
+		this.ancestors = ancestors;
+		this.mid = mid;
+		this.ti = ti;
+		this.ua = ua;
+		this.uuid = uuid;
+		this.children = new ArrayList<>();
 	}
 	
-	public void setParent(String parentPth){
-		this.parentPth = parentPth;
+	/**
+	 * set parent
+	 * @param UserPathTreeNode 
+	**/
+	public void setParent(UserPathTreeNode parentNode){
+		this.parent = parentNode;
 	}
 	
+	/**
+	 * append child to children list
+	 * @param TreeNode child
+	 */
 	public void appendChild(UserPathTreeNode child){
 		children.add(child);
-		child.setParent(ip);
+		child.setParent(this);
 	}
+	
+	
 }
