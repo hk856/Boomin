9d1d77ba952c378869f588c6e9b47c2695afb1ae
Yuyang Fan <yf8@uw.edu>
2016-10-17 22:39:02 -0400

    10s for 500 raws

--- Reconstruction.R
+++ Reconstruction.R
@@ -2,14 +2,14 @@ library(dplyr)
 library(reshape)
 library(ggplot2)
 
-#setwd("/Users/Tony")
+setwd("/Users/Tony")
 
 setwd("/Users/Heng/Documents/CornellWork/MPS_Project/GIT")
 
 
 list.files()
 
-theDataset <- read.csv(file = "data.csv", header = TRUE, sep = ",")
+theDataset <- read.csv(file = "data1.csv", header = TRUE, sep = "," , stringAsFactors=FALSE)
 
 validUsers <- filter(theDataset, uuid != "", ua != "")
 validUsers <- validUsers[order(validUsers$ip,validUsers$uuid,validUsers$ts),]
@@ -35,13 +35,19 @@ for (i in 1:length(validUsers[,1])){
   if (!is.na(index)){
     actionTable[match(0,actionTable[,index]),index] <- validUsers$sttp[i]
   }else{
-    newEntryIndex = match(0,actionTable[1,]);
+    newEntryIndex = match(0,actionTable[1,])
+    print(validUsers$uuid[i])
     actionTable[1,newEntryIndex] <- validUsers$uuid[i]#add uuid to new entry
+    print(validUsers$ua[i])
     actionTable[2,newEntryIndex] <- validUsers$ua[i]#add user agent to new entry
     actionTable[3,newEntryIndex] <- validUsers$ts[i]#add initial ts to new entry
     actionTable[4,newEntryIndex] <- -1#add -1 to final ts of the new entry
-    actionTable[4,newEntryIndex-1] <- validUsers$ts[i-1]#add last ts to the last entry
+    if(i>1){
+      actionTable[4,newEntryIndex-1] <- validUsers$ts[i-1]#add last ts to the last entry
+    }
     actionTable[5,newEntryIndex] <- validUsers$sttp[i]#add sttp to new entry
   }
 }
 actionTable[4,length(actionTable[1,])] <- validUsers$ts[length(validUsers[,1])]
+
+write.csv(actionTable, file = "testData.csv")
