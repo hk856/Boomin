45dac2e191fb3c3baeb0b80c69c05b033bddc430
hk856 <hk856@cornell.edu>
2016-11-21 21:40:20 -0500

    merging

--- RelationAnalysis.R
+++ RelationAnalysis.R
@@ -12,13 +12,26 @@ aggregatedDataset$num_priceRange_f <- NULL
 aggregatedDataset$num_orderBy_f <- NULL
 aggregatedDataset$num_custom_range <- NULL
 
-fit <- kmeans(aggregatedDataset,2)
+mydata = cbind(num_action = aggregatedDataset$num_actions,
+                  browsing_time = aggregatedDataset$browsing_time,
+                  sttp10 = aggregatedDataset$sttp10,
+                  num_filter = aggregatedDataset$num_filter,
+                  num_search = aggregatedDataset$num_search)
+
+# Determine number of clusters
+wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
+for (i in 2:15) wss[i] <- sum(kmeans(mydata, 
+                                     centers=i)$withinss)
+plot(1:15, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares")
+
+fit <- kmeans(mydata,2)
 aggregatedDataset <- data.frame(aggregatedDataset, fit$cluster)
+head(aggregatedDataset)
+fit$centers
 
-
-
-fit <- lm(aggregatedDataset$sttp10 ~ aggregatedDataset$browsing_time + aggregatedDataset$num_actions + aggregatedDataset$num_filter + aggregatedDataset$num_search)
-summary(fit) #show results
+#fit <- lm(aggregatedDataset$sttp10 ~ aggregatedDataset$browsing_time + aggregatedDataset$num_actions + aggregatedDataset$num_filter + aggregatedDataset$num_search)
+#summary(fit) #show results
 
 length(aggregatedDataset[,1])
 
